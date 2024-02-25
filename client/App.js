3471cb5f0d9d38382885b4396d6eece27fd189d1
redchipmonk <alvinlester24@gmail.com>
2024-02-24 23:31:32 -0800

    Added bar color and active/inactive icon colors

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import SomeComponent from './src/SomeComponent';
 import Outfits from './src/outfits/Outfits';
 
@@ -15,13 +16,38 @@ export default function App() {
         <Text>This is me testing a merge req</Text>
         <StatusBar style="auto" />
       </View>
-      <Tab.Navigator>
+      <BottomNavigationBar />
+    </NavigationContainer>
+  );
+}
+
+function BottomNavigationBar() {
+  return (
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: {
+            backgroundColor: '#22333B',
+          },
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'SomeComponent') {
+              iconName = 'hanger';
+            }
+            else if (route.name === 'Outfits') {
+              iconName = 'tshirt-crew-outline';
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#7F7F7F',
+          tabBarInactiveTintColor: '#FFFFFF',
+        })}
+      >
         <Tab.Screen 
           name="SomeComponent" component={SomeComponent} />
         <Tab.Screen 
           name="Outfits" component={Outfits}/>
       </Tab.Navigator>
-    </NavigationContainer>
   );
 }
 
