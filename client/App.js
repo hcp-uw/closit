021e27b0e7d44f6c273a1d6a044bfba0a85221dd
redchipmonk <alvinlester24@gmail.com>
2024-02-24 21:50:02 -0800

    Implemented basic bottom navigation bar

--- client/App.js
+++ client/App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SomeComponent from './src/SomeComponent';
+import Outfits from './src/outfits/Outfits';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is me testing how to use Expo Go!</Text>
-      <Text>This is me testing a merge req</Text>
-      <StatusBar style="auto" />
-      <SomeComponent />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>This is me testing how to use Expo Go!</Text>
+        <Text>This is me testing a merge req</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="SomeComponent" component={SomeComponent} />
+        <Tab.Screen 
+          name="Outfits" component={Outfits}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
